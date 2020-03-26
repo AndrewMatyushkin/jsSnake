e78863f979bfb8d2f31f477f18a3e2274ff74102
AndrewMatyushkin <familiaandrej@gmail.com>
2020-03-26 18:32:20 +0300

    add func eat food + snake tail

--- js/main.js
+++ js/main.js
@@ -40,8 +40,11 @@ function drawCanvas() {
     ctx.drawImage(foodImg, food.x, food.y)
 
     for(let i=0; i < snake.length; i++) {
-        ctx.fillStyle = 'green';
+        ctx.fillStyle = i == 0 ? 'green' : 'red';
         ctx.fillRect(snake[i].x,snake[i].y, box, box);
+    }
+    if('green' == 'red'){
+        console.log('1');
     }
 
     // draw text//
@@ -52,18 +55,30 @@ function drawCanvas() {
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
 
+    // eat food + create massive for snake//
     if(snakeX == food.x && snakeY == food.y) {
         score++;
         food = {
-            x: Math.floor((Math.random() * 17 + 1)) * box,
-            y: Math.floor((Math.random() * 15 + 3)) * box,
+          x: Math.floor(Math.random() * 17 + 1) * box,
+          y: Math.floor(Math.random() * 15 + 3) * box,
         };
-    }
-    else{
-        snake.pop();
+    } else {
+          snake.pop();
     }
 
-    snake.pop();
+    // out of bounds //
+    if(snakeX < box || snakeX > box * 17 || snakeY < 3 * box || snakeY > box * 17) {
+        clearInterval(game);
+    }
+
+    // eat tale//
+    function eatTail(head, arr) {
+        for(i = 0; i < arr.length; i++) {
+            if(head.x == arr[i].x && head.y == arr[i].y) {
+                clearInterval(game);
+            }
+        }
+    }
 
     if(dir == 'left') snakeX -=box;
     if(dir == 'right') snakeX +=box;
@@ -74,6 +89,7 @@ function drawCanvas() {
         x: snakeX,
         y: snakeY
     }
+    eatTail(head, snake);
     snake.unshift(head);
 }
 
