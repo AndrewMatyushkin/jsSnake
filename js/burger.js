1d2c2934ea30133b88661c756d0c65e221993b62
matyushkin <matyushkin.andrew@gmail.com>
2020-04-01 06:17:48 +0300

    new about button

--- js/burger.js
+++ js/burger.js
@@ -21,6 +21,29 @@ function burgerMenu(selector) {
         }
     }
 }
-
 burgerMenu ('.burger-menu');
+//---------------------------about-----------------------------------------------------
+var navButton = document.querySelector('.navigation-button');
+var navMenu = document.querySelector('.navigation-menu');
+var win = window;
+
+function openMenu(event) {
+  
+  navButton.classList.toggle('active');
+  navMenu.classList.toggle('active');
+
+  event.preventDefault();
+  event.stopImmediatePropagation();
+}
+  
+function closeMenu(event) {
+  if (navButton.classList.contains('active')) {
+    navButton.classList.remove('active');
+    navMenu.classList.remove('active');
+  }
+}
+  navButton.addEventListener('click', openMenu, false);
+
+win.addEventListener('click',closeMenu, false);
+    
 
